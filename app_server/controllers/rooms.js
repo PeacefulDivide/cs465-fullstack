6d233a3276cdb8892e926de4b9c326f77f47f8b1
PeacefulDivide <10346323+PeacefulDivide@users.noreply.github.com>
2026-07-16 20:24:49 -0500

    Module 3 completed baseline and two additional pages

--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,9 @@
+var fs = require('fs');
+var suite = JSON.parse(fs.readFileSync('./data/suite.json','utf8'));
+
 /* GET Rooms view */
 const rooms = (req, res) => {
-    res.render('rooms', { title: "Travlr Getaways"});
+    res.render('rooms', { title: "Travlr Getaways", suite});
 };
 
 module.exports = {
