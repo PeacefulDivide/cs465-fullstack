2fb6d18b4b903217f1f354498d08b26d40e4fa12
PeacefulDivide <10346323+PeacefulDivide@users.noreply.github.com>
2026-08-08 17:14:29 -0500

    Module 6 completed baseline

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -6,9 +6,16 @@ const tripsController = require('../controllers/trips');
 
 // Define routes for trips endpoints
 // GET method routes tripsList
-router.route('/trips').get(tripsController.tripsList);
+router
+    .route('/trips')
+    .get(tripsController.tripsList) // Get method routes tripList
+    .post(tripsController.tripsAddTrip); // Post method Adds a trip
 
 // GET method routes tripsFindByCode - require parameter
-router.route('/trips/:tripCode').get(tripsController.tripsFindByCode);
+// PUT method routes tripsUpdateTrip - requires parameter
+router
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindByCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+
+// Import controllers to route
 const ctrlMain = require('../controllers/main');
 
 /* GET home page. */
 router.get('/', ctrlMain.index);
+
 
 module.exports = router;
